d000d7ffa0408624db002b614efaf372fded162e
Dmitry Vasyliuk <dvasyliuk@atlassian.com>
2017-05-12 16:42:19 +0300

    Added ScreenObject as for "Add submodule window" and class for "Repository" menu option

---
+++ AutomationTestsSolution/Tests/SubmodulesTests.cs
@@ -0,0 +1,61 @@
+﻿using LibGit2Sharp;
+using NUnit.Framework;
+using ScreenObjectsHelpers.Helpers;
+using ScreenObjectsHelpers.Windows.Repository;
+using ScreenObjectsHelpers.Windows.MenuFolder;
+using System;
+using System.IO;
+
+namespace AutomationTestsSolution.Tests
+{
+    class SubmodulesTests : BasicTest
+    {
+        #region Test Variables
+        private string pathToClonedGitRepo = Environment.ExpandEnvironmentVariables(ConstantsList.pathToClonedGitRepo);
+        private string currentUserProfile = Environment.ExpandEnvironmentVariables(ConstantsList.currentUserProfile);
+        private string openTabsPath = Environment.ExpandEnvironmentVariables(Path.Combine(ConstantsList.pathToDataFolder, ConstantsList.opentabsXml));
+        private string resourceName = Resources.opentabs_for_clear_repo;
+        private string userprofileToBeReplaced = ConstantsList.currentUserProfile;
+        private string testString = "123";
+        private AddSubmoduleWindow addSubmoduleWindow;
+        #endregion
+
+        [SetUp]
+        public override void SetUp()
+        {
+            RemoveTestFolder();
+            CreateTestFolder();
+            Repository.Init(pathToClonedGitRepo);
+            base.BackupConfigs();
+            base.UseTestConfigAndAccountJson(sourceTreeDataPath);
+            resourceName = resourceName.Replace(userprofileToBeReplaced, currentUserProfile);
+            File.WriteAllText(openTabsPath, resourceName);
+            base.RunAndAttachToSourceTree();
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            addSubmoduleWindow.ClickCancelButton();
+            base.TearDown();
+            RemoveTestFolder();
+        }
+        private void CreateTestFolder()
+        {
+            Directory.CreateDirectory(pathToClonedGitRepo);
+        }
+        private void RemoveTestFolder()
+        {
+            Utils.RemoveDirectory(pathToClonedGitRepo);
+        }
+        [Test]
+        public void SourcePathFieldValidateCorrectGitFolderTest()
+        {
+            RepositoryTab mainWindow = new RepositoryTab(MainWindow);
+            addSubmoduleWindow = mainWindow.OpenMenu<RepositoryMenu>().OpenAddSubmoduleWindow();
+
+            addSubmoduleWindow.SourcePathTextbox.SetValue(testString);
+            
+        }
+    }
+}
