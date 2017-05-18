2900f10e8944ec86b3f7628d4b00e0c569db2842
Dmitry <dvasyliuk@atlassian.com>
2017-05-18 14:56:09 +0300

    Added 2 tests for validation submodule source

--- ScreenObjectsHelpers/Windows/Repository/AddSubmoduleWindow.cs
+++ ScreenObjectsHelpers/Windows/Repository/AddSubmoduleWindow.cs
@@ -21,9 +21,15 @@ namespace ScreenObjectsHelpers.Windows.Repository
         }
 
         #region UIItems
+        //Automation IDs required
         public TextBox SourcePathTextbox => MainWindow.Get<TextBox>(SearchCriteria.ByAutomationId("SourceTextBox"));
         public Button OKButton => MainWindow.Get<Button>(SearchCriteria.ByText("OK"));
         public Button CancelButton => MainWindow.Get<Button>(SearchCriteria.ByText("Cancel"));
+        public Label WrongPathValidationMessage => MainWindow.Get<Label>(SearchCriteria.ByControlType(ControlType.Text).AndByText("No path / URL supplied"));
+        public Label CorrectSourcePathValidationMessage => MainWindow.Get<Label>(SearchCriteria.ByControlType(ControlType.Text).AndByText("This is a Git repository"));
+        public Button AdvancedOptions => MainWindow.Get<Button>(SearchCriteria.ByAutomationId("HeaderSite"));
+        public TextBox SourceBranch => MainWindow.Get<TextBox>(SearchCriteria.ByControlType(ControlType.Edit).AndIndex(3));
+        public TextBox LocalRelativePath => MainWindow.Get<TextBox>(SearchCriteria.ByControlType(ControlType.Edit).AndIndex(2));
         #endregion
 
         #region Methods
@@ -35,6 +41,41 @@ namespace ScreenObjectsHelpers.Windows.Repository
         {
             CancelButton.Click();
             return new RepositoryTab(MainWindow);
+        }
+        public void LocalRelativePathTextboxFocus()
+        {
+            LocalRelativePath.Focus();
+        }
+        public bool IsOkButtonEnabled()
+        {
+            return OKButton.Enabled;
+        }
+        public void SetSourcePath(string value)
+        {
+            SourcePathTextbox.Text = value;
+        }
+        #endregion
+    }
+    public class NotAGitRepository
+    {
+        private UIItemContainer addSubmoduleWindow;
+        private UIItemContainer notAGitRepositoryWindow;
+        public NotAGitRepository(Window mainWindow, UIItemContainer addSubmoduleWindow, UIItemContainer notAGitRepositoryWindow)
+        {
+            this.addSubmoduleWindow = addSubmoduleWindow;
+            this.notAGitRepositoryWindow = notAGitRepositoryWindow;
+        }
+
+        #region UIItems
+        public Button CancelButton => notAGitRepositoryWindow.Get<Button>(SearchCriteria.ByText("Cancel"));
+        public TextBox ErrorMessage => notAGitRepositoryWindow.Get<TextBox>(SearchCriteria.ByText("Submodules can only be git repositories."));
+        #endregion
+
+        #region Methods
+        public UIItemContainer ClickCancelButton()
+        {
+            CancelButton.Click();
+            return addSubmoduleWindow;
         }
         #endregion
     }
