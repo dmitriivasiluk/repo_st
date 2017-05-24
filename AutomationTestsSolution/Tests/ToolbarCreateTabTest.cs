6f1fc3a554a718d98df9091144f9a1edd2ebee84
skhvalinskyi <skhvalinskyi@atlassian.com>
2017-05-24 18:40:53 +0300

    7 tests for UI and create local repo have been written. Test CheckLocalNoneRepoCreatedTest fails on issue SRCTREE-1622.

---
+++ AutomationTestsSolution/Tests/ToolbarCreateTabTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using NUnit.Framework;
+using ScreenObjectsHelpers.Helpers;
+using ScreenObjectsHelpers.Windows.ToolbarTabs;
+using ScreenObjectsHelpers.Windows;
+
+namespace AutomationTestsSolution.Tests.CreateLocal
+{
+    class ToolbarCreateTabTestLocal : BasicTest
+    {
+         #region Test Variables
+        string gitRepoName = ConstantsList.testGitRepoBookmarkName;
+        string mercurialRepoName = ConstantsList.testHgRepoBookmarkName;
+        string pathToAllRepos = Environment.ExpandEnvironmentVariables(ConstantsList.pathToCreateLocalRepos);
+        #endregion
+
+        [SetUp]
+        public override void SetUp()
+        {
+            //if folders exist for some reason
+            RemoveTestFolders(new string[] { pathToAllRepos + gitRepoName, pathToAllRepos + mercurialRepoName });
+            base.SetUp();
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+            RemoveTestFolders(new string[] { pathToAllRepos + gitRepoName, pathToAllRepos + mercurialRepoName });
+        }
+
+        [Test, Order(1)]
+        public void ValidateLocalRepoNameTest()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            createTab.DestinationPathTextBox.SetValue(pathToAllRepos + gitRepoName);
+            Assert.AreEqual(createTab.NameRepoTextBox.Text, gitRepoName);
+        }
+
+        [Test, Order(2)]
+        public void CheckCreateButtonUnavailableOnDestinationEmpty()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            //for sure
+            createTab.DestinationPathTextBox.SetValue("");
+            Assert.IsFalse(createTab.CreateButton.Visible && createTab.CreateButton.Enabled);
+        }
+
+        [Test, Order(3)]
+        public void CheckCreateButtonUnavailableOnNoRepoAccount()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            createTab.DestinationPathTextBox.SetValue(pathToAllRepos + gitRepoName);
+            createTab.CheckCheckbox(createTab.CreateRemoteCheckBox);
+            createTab.DescriptionTextBox.Focus();
+            Assert.IsFalse(createTab.CreateButton.Visible && createTab.CreateButton.Enabled);
+        }
+
+        //TODO Add dialow window verification
+        [Test, Order(4)]
+        public void CheckBrowserButtonWorks()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            Assert.IsTrue(createTab.BrowseButton.Enabled && createTab.BrowseButton.Visible);
+            Assert.DoesNotThrow(() => createTab.ClickButton(createTab.BrowseButton));
+        }
+
+        [Test]
+        public void CheckLocalGitRepoCreatedTest()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            createTab.DestinationPathTextBox.SetValue(pathToAllRepos + gitRepoName);
+            mainWindow.UncheckCheckbox(createTab.CreateRemoteCheckBox);
+            createTab.RepoTypeComboBox.Select(CreateTab.CVS.GitHub);
+            createTab.ClickButton(createTab.CreateButton);
+            Utils.ThreadWait(2000);
+            Assert.IsTrue(Directory.Exists(pathToAllRepos + gitRepoName));
+            Assert.IsTrue(Directory.Exists(pathToAllRepos + gitRepoName + "\\" + ConstantsList.dotGitFolder));
+            Assert.AreEqual(createTab.TabTextGit.Name, gitRepoName);
+        }
+
+        [Test]
+        public void CheckLocalHgRepoCreatedTest()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            createTab.DestinationPathTextBox.SetValue(pathToAllRepos + mercurialRepoName);
+            mainWindow.UncheckCheckbox(createTab.CreateRemoteCheckBox);
+            createTab.RepoTypeComboBox.Select(CreateTab.CVS.Mercurial);
+            createTab.ClickButton(createTab.CreateButton);
+            Utils.ThreadWait(2000);
+            Assert.IsTrue(Directory.Exists(pathToAllRepos + mercurialRepoName));
+            Assert.IsTrue(Directory.Exists(pathToAllRepos + mercurialRepoName + "\\" + ConstantsList.dotHgFolder));
+            Assert.AreEqual(createTab.TabTextHg.Name, mercurialRepoName);
+        }
+
+        [Test]
+        //[Ignore("Issue SRCTREE-1622")]
+        public void CheckLocalNoneRepoCreatedTest()
+        {
+            LocalTab mainWindow = new LocalTab(MainWindow);
+            CreateTab createTab = mainWindow.OpenTab<CreateTab>();
+            Assert.Throws(typeof(TestStack.White.UIItems.UIActionException), () => createTab.RepoTypeComboBox.Select(CreateTab.CVS.None));
+        }
+    }
+}
+
+namespace AutomationTestsSolution.Tests.CreateRemote
+{
+
+}
--- ScreenObjectsHelpers/Windows/ToolbarTabs/CreateTab.cs
+++ ScreenObjectsHelpers/Windows/ToolbarTabs/CreateTab.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using TestStack.White.UIItems;
+using TestStack.White.UIItems.ListBoxItems;
 using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.WindowItems;
+using ScreenObjectsHelpers.Helpers;
 
 namespace ScreenObjectsHelpers.Windows.ToolbarTabs
 {
@@ -16,6 +18,60 @@ namespace ScreenObjectsHelpers.Windows.ToolbarTabs
         {
         }
 
+        #region UIElements
+
         public override UIItem ToolbarTabButton => MainWindow.Get<UIItem>(SearchCriteria.ByText("Create"));
+        public Button BrowseButton => MainWindow.Get<Button>(SearchCriteria.ByText("Browse"));
+        public TextBox DestinationPathTextBox => MainWindow.Get<TextBox>(SearchCriteria.ByAutomationId("CreateRepoDestinationPath"));
+        public TextBox NameRepoTextBox => MainWindow.Get<TextBox>(SearchCriteria.ByAutomationId("CreateRepoName"));
+        public ComboBox RepoTypeComboBox => MainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("CreateRepoTypeDropdown"));
+        //AutomationID is needed
+        public CheckBox CreateRemoteCheckBox =>
+            MainWindow.Get<CheckBox>(SearchCriteria.ByClassName("CheckBox").AndByText("Create Repository On Account:"));
+        //=================================Opens when CreateRemoteCheckBox is checked - Start
+        public ComboBox RemoteAccountsComboBox => MainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("LocalRemoteTypeDropdown"));
+        //AutomationID is needed
+        public TextBox DescriptionTextBox => MainWindow.Get<TextBox>(SearchCriteria.ByClassName("TextBox").AndAutomationId(""));
+        public CheckBox IsPrivateCheckBox => MainWindow.Get<CheckBox>(SearchCriteria.ByClassName("CheckBox").AndByText("Is Private"));
+        //=================================Opens when CreateRemoteCheckBox is checked - End
+        public Button CreateButton => MainWindow.Get<Button>(SearchCriteria.ByClassName("Button").AndByText("CreateRepoButton"));
+        //New window is opened after successfull repo creation
+        public UIItem TabTextGit => MainWindow.Get<UIItem>(SearchCriteria.ByText(ConstantsList.testGitRepoBookmarkName));
+        public UIItem TabTextHg => MainWindow.Get<UIItem>(SearchCriteria.ByText(ConstantsList.testHgRepoBookmarkName));
+
+        #endregion
+
+        #region Methods
+        //TODO Refactor to BasicWindow
+        public Boolean IsCreateRemoteChecked()
+        {
+            return CreateRemoteCheckBox.Checked;
+        }
+        //TODO Refactor to BasicWindow
+        public Boolean IsPrivateChecked()
+        {
+            return IsPrivateCheckBox.Checked;
+        }
+
+        public Boolean IsRepoSettingsAvailable()
+        {
+            CheckCheckbox(CreateRemoteCheckBox);
+            Utils.ThreadWait(1000);
+            return RemoteAccountsComboBox.Enabled && RemoteAccountsComboBox.Visible &&
+                DescriptionTextBox.Enabled && DescriptionTextBox.Visible &&
+                IsPrivateCheckBox.Enabled && IsPrivateCheckBox.Visible;
+        }
+        
+        #endregion
+
+        //TODO: Put this struct to  ScreenObjectsHelpers / Windows / ToolbarTabs / 
+        //EditHostingAccountWindow / EditHostingAccountWindow.cs
+        public struct CVS
+        {
+            public static string GitHub = "Git";
+            public static string Mercurial = "Mercurial";
+            //somesing strange
+            public static string None = "None";
+        }
     }
 }
