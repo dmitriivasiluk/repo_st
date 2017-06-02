78ce5b16f3e245589d2f14e5566d0b735c57e9b0
Dmitry Vasyliuk <dvasyliuk@atlassian.com>
2017-06-02 22:05:43 +0300

    Code refactoring (+1 squashed commits)
    Squashed commits:
    
    [593e2e4] Added implicit wait to prevent test failing due to slow parsing of some fields

--- AutomationTestsSolution/Tests/SubtreesTests.cs
+++ AutomationTestsSolution/Tests/SubtreesTests.cs
@@ -6,6 +6,7 @@ using ScreenObjectsHelpers.Windows.MenuFolder;
 using System;
 using System.IO;
 using static ScreenObjectsHelpers.Windows.MenuFolder.RepositoryMenu;
+using ScreenObjectsHelpers.Windows;
 
 namespace AutomationTestsSolution.Tests
 {
@@ -75,7 +76,8 @@ namespace AutomationTestsSolution.Tests
             addLinkSubtree.SetTextboxContent(addLinkSubtree.LocalRelativePathTextbox, testString);
             Utils.ThreadWait(3000);
             addLinkSubtree.SetTextboxContent(addLinkSubtree.BranchCommitTextbox, testString);
-            Utils.ThreadWait(2000);
+            addLinkSubtree.SourcePathTextbox.Focus();
+            Utils.ThreadWait(3000);
 
             Assert.IsTrue(addLinkSubtree.IsOkButtonEnabled());
         }
