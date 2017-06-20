298ab9a6aa481ac3a24eec0b4ea73b865dcadf3d
Dmitry Vasyliuk <dvasyliuk@atlassian.com>
2017-06-20 12:40:17 +0300

    Code refactoring + changes to yaml

--- ScreenObjectsHelpers/Helpers/AttemptsCounterLogger.cs
+++ ScreenObjectsHelpers/Helpers/AttemptsCounterLogger.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScreenObjectsHelpers.Helpers
 {
     public class AttemptsCounterLogger
     {
-        public static void AttemptCounter(string nameOfMethod, string testName, int counter)
+        public static void AttemptCounter(string nameOfMethod, string testContextFullName, int counter)
         {
-            var output = Environment.NewLine + testName + " -> " + nameOfMethod + " -> " + counter;
-            
+            //var output = Environment.NewLine + testContextFullName + " -> " + nameOfMethod + " -> " + counter + " -> [" + DateTime.Now.ToString("h:mm:ss tt");
+
+            string output = string.Format("[{0}] {1} -> {2} -> Attempts: {3}", DateTime.Now.ToString("h:mm:ss"), testContextFullName, nameOfMethod, counter) + Environment.NewLine;
             string path = Environment.ExpandEnvironmentVariables(@"%userprofile%\Documents\attemptsCounter.txt");
 
             File.AppendAllText(path, output);
-            
         }
     }
 }
