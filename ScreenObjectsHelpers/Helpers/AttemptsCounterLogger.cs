903c9fe9febe61f42b312fddd9f5fbc2de7ed81e
Dmitry Vasyliuk <dvasyliuk@atlassian.com>
2017-06-20 13:40:06 +0300

    Refactoring

--- ScreenObjectsHelpers/Helpers/AttemptsCounterLogger.cs
+++ ScreenObjectsHelpers/Helpers/AttemptsCounterLogger.cs
@@ -7,9 +7,8 @@ namespace ScreenObjectsHelpers.Helpers
     {
         public static void AttemptCounter(string nameOfMethod, string testContextFullName, int counter)
         {
-            //var output = Environment.NewLine + testContextFullName + " -> " + nameOfMethod + " -> " + counter + " -> [" + DateTime.Now.ToString("h:mm:ss tt");
+            string output = string.Format("[{0}] {1} -> {2} -> Attempts: {3}", DateTime.Now.ToString("h:mm:ss dd.MM.yyyy"), testContextFullName.Replace("AutomationTestsSolution.Tests.", ""), nameOfMethod, counter) + Environment.NewLine;
 
-            string output = string.Format("[{0}] {1} -> {2} -> Attempts: {3}", DateTime.Now.ToString("h:mm:ss"), testContextFullName, nameOfMethod, counter) + Environment.NewLine;
             string path = Environment.ExpandEnvironmentVariables(@"%userprofile%\Documents\attemptsCounter.txt");
 
             File.AppendAllText(path, output);
